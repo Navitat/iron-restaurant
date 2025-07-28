70776faf259557687db6dd6c04b495694d33cc11
navitat <anlon636@gmail.com>
2025-07-28 12:37:22 +0200

    feat: add views, sendFile of html files

--- app.js
+++ app.js
@@ -10,21 +10,18 @@ app.use(express.static("public"));
 // GET /
 app.get("/", (req, res, next) => {
   console.log("received request to the root path...");
-  res.send(`
-    <link rel="stylesheet" href="/css/main.css" />
-    <h1>This is the HomePage</h1>
-    <h2>Oh wow</h2>
-    <img src="/images/home.jpg" alt="image of pizza slices"/>
-    `);
+  // res.send();
+  res.sendFile(__dirname + "/views/home.html");
 });
 
 // GET /contact
 app.get("/contact", (req, res, next) => {
   console.log("received request to the contact page...");
-  res.send("<h1>This is the Contact page</h1>");
+  // res.send();
+  res.sendFile(__dirname + "/views/contact.html");
 });
 
-// Define port
+// Define and listen port
 app.listen(PORT, () => {
   console.log(`APP RUNNING on PORT: ${PORT}`);
 });
